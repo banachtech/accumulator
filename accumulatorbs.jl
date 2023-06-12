e1a30df7a045dc69081937b44b3985c69013f063
LouisHoo5227 <E190003@e.ntu.edu.sg>
2023-06-12 15:57:10 +0800

    publish

--- accumulatorbs.jl
+++ accumulatorbs.jl
@@ -11,7 +11,7 @@ Base.@kwdef mutable struct ArgsBlackScholes
     num_settlements = 12
     settlement_offset = 2
     gte_periods = 1
-    spot_price = 100.0
+    spot_price = 100
     strike_price = 90
     ko_price = 105
 end
@@ -33,28 +33,32 @@ function valueAccumulator()
     S = args.spot_price
     t = []
     T = []
+    m = []
     beta = 0.5826
-    for i in 1:length(dates)
-        for j in 1:length(dates[i].obs_dates)
-            push!(t, Dates.value(dates[i].obs_dates[1] - args.effective_date)/365)
-            push!(T, Dates.value(dates[i].settle_date - args.effective_date)/365)
+    for i in eachindex(dates)
+        for j in eachindex(dates[i].obs_dates)
+            push!(m, length(dates[i].obs_dates))
+            push!(t, Dates.value(dates[i].obs_dates[j] - args.trade_date)/365)
+            push!(T, Dates.value(dates[i].settle_date - args.trade_date)/365)
         end
     end
-    mu = mkt.rf - mkt.div - mkt.σ^2/2
-    lambda = 1 + mu / mkt.σ^2
+    μ = mkt.rf - mkt.div - 0.5 * mkt.σ^2
+    λ = 1 + μ / (mkt.σ^2)
 
-    x(t) = (log(S/K)+ (mu +  mkt.σ^2)*t)/ mkt.σ * sqrt(t)
-    x1(t) = (log(S/H)+ (mu +  mkt.σ^2)*t)/ mkt.σ * sqrt(t)
-    y(t) = (log(H^2/S/K) + (mu+ mkt.σ^2)*t)/ mkt.σ * sqrt(t)
-    y1(t) = (log(H/S) + (mu+ mkt.σ^2)*t)/ mkt.σ * sqrt(t)
+    x(t) = (log(S/K)+ (μ +  mkt.σ^2)*t)/ (mkt.σ * sqrt(t))
+    x1(t) = (log(S/H)+ (μ +  mkt.σ^2)*t)/ (mkt.σ * sqrt(t))
+    y(t) = (log(H^2/(S*K)) + (μ+ mkt.σ^2)*t)/ (mkt.σ * sqrt(t))
+    y1(t) = (log(H/S) + (μ + mkt.σ^2)*t)/ (mkt.σ * sqrt(t))
 
     N(x) = cdf(Normal(), x)
     value = 0
-    for i in 1:length(t)
-        H = args.ko_price * exp(beta * mkt.σ * sqrt((Dates.value(periods[i] - args.trade_date))/365/i))
+    for i in eachindex(t)
+        H = args.ko_price * exp(beta * mkt.σ * sqrt(T[i] / m[i]))
         value = value + 
-        (S*exp(-mkt.div*T[i]))* ((2 - N(x(t[i])) -                  N(x1(t[i])) -                   (H/S)^(2*lambda)    * N(-y(t[i]))                   - (H/S)^(2*lambda) *    N(-y1(t[i])))) - 
-        (K*exp(-mkt.rf*T[i]))*  ((2 - N(x(t[i])-mkt.σ*sqrt(t[i]))-  N(x1(t[i])-mkt.σ*sqrt(t[i]))) - (H/S)^(2*lambda-2)  * N(-y(t[i])+mkt.σ*sqrt(t[i]))  - (H/S)^(2*lambda-2) *  N(-y1(t[i])+mkt.σ*sqrt(t[i])))
+        (S * exp(-mkt.div * T[i])) * ((2 - N(x(t[i])) -                  N(x1(t[i])) -                   (H/S)^(2*λ)    * N(-y(t[i]))                   - (H/S)^(2*λ) *    N(-y1(t[i])))) - 
+        (K * exp(-mkt.rf * T[i])) *  ((2 - N(x(t[i])-mkt.σ*sqrt(t[i]))-  N(x1(t[i])-mkt.σ*sqrt(t[i]))) - (H/S)^(2*λ-2)  * N(-y(t[i])+mkt.σ*sqrt(t[i]))  - (H/S)^(2*λ-2) *  N(-y1(t[i])+mkt.σ*sqrt(t[i])))
     end
     return value
 end
+
+println(valueAccumulator())
